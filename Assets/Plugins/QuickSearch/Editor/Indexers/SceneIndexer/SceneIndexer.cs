cad5981aec26574a9d47c7241aca2f74be50ecff
Heejin <monster@teamappetizer.com>
2016-09-19 20:26:36 +0900

    Make sceneIndexer to be able to find inactive gameObjects

--- Assets/Plugins/QuickSearch/Editor/Indexers/SceneIndexer/SceneIndexer.cs
+++ Assets/Plugins/QuickSearch/Editor/Indexers/SceneIndexer/SceneIndexer.cs
@@ -9,14 +9,40 @@ namespace QuickSearch {
 	public sealed class SceneIndexer : SearchIndexerBase {
 		private readonly List<ISearchableElement> elements_ = new List<ISearchableElement>(100);
 
+		private readonly List<Transform> tempTransforms_ = new List<Transform>(100);
+
 		protected override void OnOpen () {
 			elements_.Clear();
 
-			var allGameObjects = GameObject.FindObjectsOfType<GameObject>();
-			for (var i = 0; i < allGameObjects.Length; ++i) {
-				var element = new SceneSearchableElement(allGameObjects[i]);
-				elements_.Add(element);
+			var rootObjs = GetSceneRoots();
+			for (var i = 0; i < rootObjs.Count; ++i) {
+				var obj = rootObjs[i];
+				obj.GetComponentsInChildren<Transform>(true, tempTransforms_);
+
+				for (var j = 0; j < tempTransforms_.Count; ++j) {
+					var tr = tempTransforms_[j];
+					if (tr == null)
+						continue;
+
+					var element = new SceneSearchableElement(tr.gameObject);
+					elements_.Add(element);
+				}
 			}
+		}
+
+		private static List<GameObject> GetSceneRoots () {
+			var prop = new HierarchyProperty(HierarchyType.GameObjects);
+			var expanded = new int[0];
+			var roots = new List<GameObject>();
+
+			while (prop.Next(expanded)) {
+				var go = prop.pptrValue as GameObject;
+				if (go == null)
+					continue;
+				roots.Add(go);
+			}
+
+			return roots;
 		}
 
 		protected override List<ISearchableElement> GetElements () {
